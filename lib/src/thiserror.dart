f6a35e111300c8c4d46f41881655d33b2619fb98
mcmah309 <dillonmcmahon79@gmail.com>
2024-03-08 06:27:09 +0000

    feat: Add ThisError class

---
+++ lib/src/thiserror.dart
@@ -0,0 +1,45 @@
+
+abstract class ThisError<T extends ThisError<T>> {
+  static final _innerNumberCapture = RegExp(r'{(\d+)}');
+
+  final String? template;
+  final List<Object>? values;
+
+  const ThisError([this.template, this.values]);
+
+  @override
+  String toString() {
+    if (values == null) {
+      if (template == null) {
+        return "$T";
+      } else {
+        return "$T: $template";
+      }
+    }
+    if (template == null) {
+      return "$T";
+    }
+    final replacedTemplate = template!.replaceAllMapped(_innerNumberCapture, (Match match) {
+      int index = int.parse(match.group(1)!);
+      if (index < values!.length) {
+        return values![index].toString();
+      } else {
+        // If index is out of bounds, keep the original placeholder
+        return match.group(0)!;
+      }
+    }).trim();
+    if (replacedTemplate.isEmpty) {
+      return "$T";
+    } else {
+      return "$T: $replacedTemplate";
+    }
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return runtimeType == other.runtimeType;
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+}
